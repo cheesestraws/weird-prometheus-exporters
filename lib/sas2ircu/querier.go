392a3198330db4337a83b0dfd24d8eb392834c8d
cheesestraws <rob@midworld.co.uk>
2024-08-07 23:14:41 +0100

    basic querier

---
+++ lib/sas2ircu/querier.go
@@ -0,0 +1,42 @@
+package sas2ircu
+
+import (
+	"os/exec"
+)
+
+type Querier struct {
+	Executable string
+}
+
+type Response struct {
+	Adapters map[string]SAS2IRCUAdapter
+	Devices map[string]Devices
+}
+
+func (q *Querier) getBytes(args ...string) ([]byte, error) {
+	return exec.Command(q.Executable, args...).Output()
+}
+
+func (q *Querier) Get() (*Response, error) {
+	r := &Response{}
+	
+	bs, err := q.getBytes("LIST")
+	if err != nil {
+		return nil, err
+	}
+	
+	r.Adapters = parseSAS2IRCUList(bs)
+	r.Devices = make(map[string]Devices)
+	
+	for id := range r.Adapters {
+		bs, err := q.getBytes(id, "DISPLAY")
+		if err != nil {
+			return nil, err
+		}
+		
+		devs := parseSAS2IRCUDisplay(bs)
+		r.Devices[id] = devs
+	}
+	
+	return r, nil
+}
