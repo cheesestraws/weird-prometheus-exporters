146c523972333b0f5e74e9995858de36ffc9bde8
cheesestraws <rob@midworld.co.uk>
2026-03-18 23:43:18 +0000

    mdns_exporter: prom types

--- cmd/mdns_exporter/prom.go
+++ cmd/mdns_exporter/prom.go
@@ -1,1 +1,10 @@
 package main
+
+type Service struct {
+	Name string `prometheus_label:"name"`
+	Desc string `prometheus_label:"description"`
+}
+
+type Output struct {
+	ServiceLastSeen map[Service]int `prometheus_map:"service_last_seen"`
+}
